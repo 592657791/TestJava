fe56fafbd340c02c6fc2b7dd2c903ddba044e172
an <592657791@qq.com>
2018-01-07 22:35:40 +0800

    Semaphore信号量

---
+++ src/main/java/com/an/java/TestJava/TestSemaphore.java
@@ -0,0 +1,37 @@
+package com.an.java.TestJava;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 在信号量上我们定义两种操作：
+ * acquire（获取） 当一个线程调用acquire操作时，它要么通过成功获取信号量（信号量减1），要么一直等下去，直到有线程释放信号量，或超时。
+ * release（释放）实际上会将信号量的值加1，然后唤醒等待的线程。
+ * 
+ * 信号量主要用于两个目的，一个是用于多个共享资源的互斥使用，另一个用于并发线程数的控制。
+ * @author Administrator
+ *
+ */
+public class TestSemaphore {
+	public static void main(String[] args) {
+		//模拟三个停车位
+		Semaphore semaphore = new Semaphore(3);
+		
+		//模拟有6辆车,去抢3个车位
+		for(int i=1;i<=6;i++) {
+			new Thread(()->{
+				try {
+					semaphore.acquire();
+					System.out.println(Thread.currentThread().getName()+"\t"+"抢到车位");
+					TimeUnit.SECONDS.sleep(new Random().nextInt(10));
+					System.out.println(Thread.currentThread().getName()+"\t"+"离开车位");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}finally {
+					semaphore.release();
+				}
+			}, String.valueOf(i)).start();
+		}
+	}
+}
